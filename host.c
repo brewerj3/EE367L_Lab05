2fc37795f549c029637d27400f44cbe4c91aac3a
brewerj3 <brewerj3@hawaii.edu>
2023-03-20 16:33:49 -1000

    breaking files into multiple packets should work now

--- host.c
+++ host.c
@@ -504,24 +504,48 @@ _Noreturn void host_main(int host_id) {
                             new_packet->type = PKT_FILE_UPLOAD_END;
 
                             // Find the size of the file
-                            FILE* fp2 = fp;
+                            FILE *fp2 = fp;
                             fseek(fp2, 0L, SEEK_END);
                             int fileSize = (int) ftell(fp2);
                             free(fp2);
 
                             // if file size is larger than PKT_PAYLOAD_MAX we need to break it up into several packets
-                            if(fileSize < PKT_PAYLOAD_MAX) {
-                                int numberOfPackets = (fileSize - 100) / (100); // should contain the number of packets needed to send the file
-                                if(fileSize > 1000) {
+                            if (fileSize > PKT_PAYLOAD_MAX) {
+                                int numberOfPackets = (fileSize - 100) /
+                                                      (100); // should contain the number of packets needed to send the file
+                                if (fileSize > 1000) {
                                     numberOfPackets = 9;
                                 }
+                                struct packet **middle_packet = (struct packet **) malloc(
+                                        sizeof(struct packet) * numberOfPackets);
+                                struct host_job **middle_jobs = (struct host_job **) malloc(
+                                        sizeof(struct host_job) * numberOfPackets);
+                                for (int j = 0; j < numberOfPackets; j++) {
+                                    // Create array of packets for middle of files
+                                    middle_packet[j]->dst = new_job->file_upload_dst;
+                                    middle_packet[j]->src = (char) host_id;
+                                    middle_packet[j]->type = PKT_FILE_UPLOAD_MIDDLE;
 
-                                struct packet *middle_packet = (struct packet *) malloc(sizeof(struct packet));
-                                middle_packet->dst = new_job->file_upload_dst;
-                                middle_packet->src = (char) host_id;
-                                middle_packet->type = PKT_FILE_UPLOAD_MIDDLE;
-                                // @TODO Add in sending the middle packets here
+                                    // Add packets to jobs
+
+
+                                    // Read from file
+                                    n = fread(string, sizeof(char), PKT_PAYLOAD_MAX, fp);
+                                    for (int x = 0; x < n; x++) {
+                                        middle_packet[j]->payload[x] = string[x];
+                                    }
+
+                                    middle_packet[j]->length = n;
+
+                                    // Create middle jobs to send the packet and put the job in queue
+                                    middle_jobs[i]->type = JOB_SEND_PKT_ALL_PORTS;
+                                    middle_jobs[i]->packet = middle_packet[i];
+                                    job_q_add(&job_q, middle_jobs[i]);
+
+                                }
+
                                 free(middle_packet);
+                                free(middle_jobs);
                             }
 
                             // This is what actually reads the file into the packet
