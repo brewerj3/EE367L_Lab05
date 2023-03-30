53f487a94c2a50d296bdd126c26a60881f6c0a8a
Jason Enojardo <62962675+JasonEnojardo@users.noreply.github.com>
2023-03-30 09:49:21 -1000

    Create switch.h

---
+++ switch.h
@@ -0,0 +1,12 @@
+#ifndef SWITCH_H
+#define SWITCH_H
+
+#include "net.h"
+
+/* Initialize the switch node */
+void switch_init(int id);
+
+/* The main routine for the switch node */
+void switch_main(int id);
+
+#endif
