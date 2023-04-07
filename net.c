7afd296cb762947ceb0a2017b5c86eae0e03266a
brewerj3 <100174522+brewerj3@users.noreply.github.com>
2023-04-06 18:26:04 -1000

    working on adding sockets

--- net.c
+++ net.c
@@ -23,6 +23,8 @@
 #include <unistd.h>
 #include <netdb.h>
 #include <signal.h>
+#include <errno.h>
+#include <sys/wait.h>
 
 #include "main.h"
 #include "man.h"
@@ -48,8 +50,8 @@ struct net_link {
     enum NetLinkType type;
     int pipe_node0;
     int pipe_node1;
-    int tcp_socket_send;
-    int tcp_socket_recv;
+    char tcp_socket_send[NAME_LENGTH];
+    char tcp_socket_recv[NAME_LENGTH];
     char myDomain[NAME_LENGTH];
     char connectedDomain[NAME_LENGTH];
 };
@@ -323,8 +325,6 @@ void create_man_ports(struct man_port_at_man **p_man, struct man_port_at_host **
 
             p_h->next = *p_host;
             *p_host = p_h;
-        } else if (p->type == SOCKET) {
-            // @TODO make this work
         }
     }
 
@@ -345,7 +345,16 @@ void create_node_list() {
     }
 
 }
+void sigchld_handler(int s) {
+    (void) s; // quiet unused variable warning
 
+    // waitpid() might overwrite errno, so we save and restore it:
+    int saved_errno = errno;
+
+    while (waitpid(-1, NULL, WNOHANG) > 0);
+
+    errno = saved_errno;
+}
 
 /*
  * Create links, each with either a pipe or socket.
@@ -393,13 +402,66 @@ void create_port_list() {
             g_port_list = p0;
 
         } else if (g_net_link[i].type == SOCKET) {
-            struct addrinfo hints, s0, s1;
-            struct sockaddr_storage their_addr;
+            // @TODO make this work
+            int sockfd, new_fd, numbytes;  // listen on sock_fd, new connection on new_fd
+            struct addrinfo hints, *servinfo, *p;
+            struct sockaddr_storage their_addr; // connector's address information
             socklen_t sin_size;
             struct sigaction sa;
+            int yes = 1;
             char s[INET6_ADDRSTRLEN];
+            char buff[NAME_LENGTH];
 
-        }
+            int rv;
+
+            memset(&hints, 0, sizeof hints);
+            hints.ai_family = AF_UNSPEC;
+            hints.ai_socktype = SOCK_STREAM;
+            hints.ai_flags = AI_PASSIVE; // use my IP
+
+            if ((rv = getaddrinfo(NULL, g_net_link[i].tcp_socket_recv, &hints, &servinfo)) != 0) {
+                fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
+                exit(1);
+            }
+            // loop through all the results and bind to the first we can
+            for (p = servinfo; p != NULL; p = p->ai_next) {
+                if ((sockfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) == -1) {
+                    perror("server: socket");
+                    continue;
+                }
+
+                if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) == -1) {
+                    perror("setsockopt");
+                    exit(1);
+                }
+
+                if (bind(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
+                    close(sockfd);
+                    perror("server: bind");
+                    continue;
+                }
+
+            }
+            freeaddrinfo(servinfo); // all done with this structure
+
+            if (p == NULL) {
+                fprintf(stderr, "server: failed to bind\n");
+                exit(1);
+            }
+
+            if (listen(sockfd, 10) == -1) {
+                perror("listen");
+                exit(1);
+            }
+
+            sa.sa_handler = sigchld_handler; // reap all dead processes
+            sigemptyset(&sa.sa_mask);
+            sa.sa_flags = SA_RESTART;
+            if (sigaction(SIGCHLD, &sa, NULL) == -1) {
+                perror("sigaction");
+                exit(1);
+            }
+
     }
 
 }
@@ -479,7 +541,7 @@ int load_net_data_file() {
     char link_type;
     int node0, node1;
     char *domain0, *domain1;
-    int port0, port1;
+    char *port0, *port1;
 
     fscanf(fp, " %d ", &link_num);
     printf("Number of links = %d\n", link_num);
@@ -499,11 +561,11 @@ int load_net_data_file() {
                 g_net_link[i].pipe_node0 = node0;
                 g_net_link[i].pipe_node1 = node1;
             } else if(link_type == 'S') {   // Create a socket
-                fscanf(fp, " %d %s %d %s %d", &node0, domain0, &port0, domain1, &port1);
+                fscanf(fp, " %d %s %s %s %s", &node0, domain0, &port0, domain1, &port1);
                 g_net_link[i].type = SOCKET;
-                g_net_link[i].tcp_socket_recv = node0;  // Socket to listen to
-                g_net_link[i].tcp_socket_send = node1;  // Socket to send to
                 for(int k = 0; k < NAME_LENGTH; k++) {
+                    g_net_link[i].tcp_socket_recv[k] = port0[k];  // Socket to listen to
+                    g_net_link[i].tcp_socket_send[k] = port1[k];  // Socket to send to
                     g_net_link[i].myDomain[k] = domain0[k];         // Domain to listen on
                     g_net_link[i].connectedDomain[k] = domain1[k];  // Domain to send to
                 }
