2e2245031a0ce7222e25e477f09f2cb1bb341dff
brewerj3 <brewerj3@hawaii.edu>
2023-07-20 16:33:17 -1000

    added doxygen commenting to server.h

--- src/server.h
+++ src/server.h
@@ -3,6 +3,7 @@
 
 #define NAMING_TABLE_SIZE 256
 
+/// Server job queue, used by the server to determine what job to execute
 struct server_job_queue {
     struct server_job *head;
     struct server_job *tail;
@@ -13,6 +14,7 @@ struct server_job_queue {
 
 //int job_q_num(struct server_job_queue *j_q);
 
+/// The main function of the server, loops until killed by the manager
 _Noreturn void server_main(int host_id);
 
 #endif //EE367L_LAB05_SERVER_H
